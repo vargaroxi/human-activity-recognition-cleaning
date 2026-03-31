05212b24af60027c455a9ef89d6db47e5bf3bad8
vargaroxi <roxivarga@gmail.com>
2026-03-31 20:12:33 +0200

    Updated with some modification on the variable nameing, the README file and the CodeBook file

--- run_analysis.R
+++ run_analysis.R
@@ -3,29 +3,29 @@ library(dplyr)
 library(stringr)
 
 # Read the data
-features <- "./UCI HAR Dataset/features.txt"
-featureVector <- read.table(features, header = FALSE)
+featuresPath <- "./UCI HAR Dataset/features.txt"
+featureVector <- read.table(featuresPath, header = FALSE)
 
-activity <- "./UCI HAR Dataset/activity_labels.txt"
-activityLabels <- read.table(activity, header = FALSE)
+activityPath <- "./UCI HAR Dataset/activity_labels.txt"
+activityLabels <- read.table(activityPath, header = FALSE)
 
-subjectTest <- "./UCI HAR Dataset/test/subject_test.txt"
-subjectTestIndex <- read.table(subjectTest, header = FALSE)
+subjectTestPath <- "./UCI HAR Dataset/test/subject_test.txt"
+subjectTestIndex <- read.table(subjectTestPath, header = FALSE)
 
-XTest <- "./UCI HAR Dataset/test/X_test.txt"
-XTestData <- read.table(XTest, header = FALSE)
+XTestPath <- "./UCI HAR Dataset/test/X_test.txt"
+XTestData <- read.table(XTestPath, header = FALSE)
 
-YTest <- "./UCI HAR Dataset/test/Y_test.txt"
-YTestData <- read.table(YTest, header = FALSE)
+YTestPath <- "./UCI HAR Dataset/test/Y_test.txt"
+YTestData <- read.table(YTestPath, header = FALSE)
 
-subjectTrain <- "./UCI HAR Dataset/train/subject_train.txt"
-subjectTrainIndex <- read.table(subjectTrain, header = FALSE)
+subjectTrainPath <- "./UCI HAR Dataset/train/subject_train.txt"
+subjectTrainIndex <- read.table(subjectTrainPath, header = FALSE)
 
-XTrain <- "./UCI HAR Dataset/train/X_train.txt"
-XTrainData <- read.table(XTrain, header = FALSE)
+XTrainPath <- "./UCI HAR Dataset/train/X_train.txt"
+XTrainData <- read.table(XTrainPath, header = FALSE)
 
-YTrain <- "./UCI HAR Dataset/train/Y_train.txt"
-YTrainData <- read.table(YTrain, header = FALSE)
+YTrainPath <- "./UCI HAR Dataset/train/Y_train.txt"
+YTrainData <- read.table(YTrainPath, header = FALSE)
 
 # Merge the training and test data
 measurementData <- rbind(XTestData, XTrainData) 
@@ -35,8 +35,8 @@ rm(XTestData, XTrainData, subjectTestIndex, subjectTrainIndex, YTestData, YTrain
 
 # extract only the mean and std for each measurement and name the variables 
 names(measurementData) <- featureVector[,2]
-cols <- grep("mean\\(\\)|std\\(\\)", names(measurementData))
-measurementData <- measurementData[,cols]
+cols <- grepl("mean\\(\\)|std\\(\\)", names(measurementData))
+measurementData <- measurementData[, cols]
 
 names(measurementData) <- names(measurementData) %>% 
   str_replace("^t", "time_") %>% 
@@ -49,18 +49,24 @@ names(measurementData) <- names(measurementData) %>%
   str_replace_all("Mag", "magnitude_") %>%
   str_replace_all("mean\\(\\)", "mean_") %>%
   str_replace_all("std\\(\\)", "std_") %>%
-  str_replace_all("-", "")
+  str_replace_all("-", "") %>% 
+  str_remove("_$") %>% 
+  tolower()
 
 # Name the subject vector
 names(subjects) <- "subject"
 
 # Use descriptive activity names
 featureMap <- setNames(activityLabels[[2]], activityLabels[[1]])
-activity <- featureMap[as.character(activity[,1])]
+activity <- featureMap[as.character(activity[[1]])]
 
 # Merge all the columns together and convert the df to tibble
-measurementData <- as_tibble(cbind(subjects, activity, measurementData))
+measurementData <- bind_cols(subjects, tibble(activity = activity), measurementData)
 
 # Create a second summary table
-measurementSummary  <- measurementData %>% group_by(subject, activity) %>% summarize(across(-(1:2), \(x) mean(x, na.rm = TRUE) ))
+measurementSummary  <- measurementData %>% 
+  group_by(subject, activity) %>% 
+  summarize(
+    across(where(is.numeric), \(x) mean(x, na.rm = TRUE)), .groups = "drop")
+                                                                                
 write.table(measurementSummary, "measurementSummary.txt", row.names = FALSE)
